9f4bfffb7b73ec38cd85f91fad96deadf3cfc9d3
elChupaCambra <70155116+elChupaCambra@users.noreply.github.com>
2020-12-11 20:23:54 +0100

    Update hangman.js

--- hangman.js
+++ hangman.js
@@ -5,7 +5,7 @@ Wikimedia foundation for the nice pictures
 Creators of an-array-of-english-words for the random words
 */
 
-const wordArray = [];
+var wordArray = [];
 
 fetch("./words.json").then(a => a.json())
   .then(data => wordArray = data)

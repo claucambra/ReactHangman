f43907b721cb631c8325202002a19c0a959e26a6
elChupaCambra <70155116+elChupaCambra@users.noreply.github.com>
2020-12-11 20:19:23 +0100

    Update hangman.js

--- hangman.js
+++ hangman.js
@@ -10,7 +10,7 @@ const wordArray = [];
 function loadJSON(callback) {   
   var xobj = new XMLHttpRequest();
   xobj.overrideMimeType("application/json");
-  xobj.open('GET', 'words.json', true);
+  xobj.open('GET', './words.json', true);
   xobj.onreadystatechange = function () {
     if (xobj.readyState == 4 && xobj.status == "200") {
       callback(JSON.parse(xobj.responseText));
